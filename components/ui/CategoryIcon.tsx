64e495b7ea832728fecba0bc570881e2e49832ab
Bryan Joseph <bryanllana14@gmail.com>
2024-04-02 01:51:55 -0500

    highlight link

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,6 +1,8 @@
+"use client"
 import { Category } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 type Props = {
   category: Category
@@ -8,8 +10,10 @@ type Props = {
 
 const CategoryIcon = ({ category }: Props) => {
   const { name, slug } = category
+  const params = useParams()
+
   return (
-    <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b hover:bg-yellow-500`}>
+    <div className={`${slug === params.category ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b hover:bg-amber-400`}>
       <div className="w-16 h-16 relative">
         <Image src={`/icon_${slug}.svg`} alt={`Categoría ${name}`} fill />
       </div>
