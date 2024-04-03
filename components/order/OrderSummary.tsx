aafbbb08c8c51e1679cf6d3f5d0eb03a64fb2d5c
Bryan Joseph <bryanllana14@gmail.com>
2024-04-03 13:14:43 -0500

    Total show in order summary

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { useStore } from "@/src/store"
 import ProductDetails from "./ProductDetails"
+import { useMemo } from "react"
+import { formatPriceToPen } from "@/src/helpers"
 
 const OrderSummary = () => {
   const order = useStore(state => state.order)
+  const total = useMemo(() => order.reduce((total, item) => item.subtotal + total, 0), [order])
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl font-black text-center">Mi pedido</h1>
@@ -15,6 +18,11 @@ const OrderSummary = () => {
           { order.map(item => (
             <ProductDetails key={item.id} item={item} />
           ))}
+
+          <p className="text-2xl mt-10 text-center">
+            Total a pagar:
+            <span className="font-bold"> { formatPriceToPen(total) }</span>
+          </p>
         </div>
       )}
     </aside>
