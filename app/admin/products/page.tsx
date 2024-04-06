eeaf985d6dffd103d0bc147ec7396f039d6a4b98
Bryan Joseph <bryanllana14@gmail.com>
2024-04-06 12:45:55 -0500

    Get products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,23 @@
-const ProductsPage = () => {
+import ProductTable from "@/components/products/ProductTable"
+import { prisma } from "@/src/lib/prisma"
+
+const getProducts = async () => {
+  const products = await prisma.product.findMany({
+    include: {
+      category: true
+    }
+  })
+  return products
+}
+
+const ProductsPage = async () => {
+  const products = await getProducts()
+
   return (
     <>
       <h1 className="text-2xl my-10">Administrar productos</h1>
+
+      <ProductTable  products={products}/>
     </>
   )
 }
