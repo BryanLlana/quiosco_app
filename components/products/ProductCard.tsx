8acae4f0d15b8356dc78a028f7c90414780b4297
Bryan Joseph <bryanllana14@gmail.com>
2024-04-13 01:18:44 -0500

    Image cloudinary fix

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -11,7 +11,13 @@ const ProductCard = ({ product }: Props) => {
   const { name, price, image } = product
   return (
     <div className="border bg-white">
-      <Image src={`/products/${image}.jpg`} alt={`Product ${name}`} width={400} height={500} quality={100} />
+      <Image
+        src={`${image.startsWith('https://res.cloudinary.com') ? image : `/products/${image}.jpg`}`}
+        alt={`Product ${name}`} 
+        width={400} 
+        height={500} 
+        quality={100}
+      />
       <div className="p-5">
         <h3 className="text-2xl font-bold">{ name }</h3>
         <p className="mt-5 font-black text-4xl text-amber-500">
